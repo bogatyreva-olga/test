9089d4fb7085402f0df36688dae7f2e8516b578f
bogatyreva <bogatyreva.o.v@gmail.com>
2021-04-07 18:16:45 +0300

    add function addNumberButtons

--- input-number/js/main.js
+++ input-number/js/main.js
@@ -12,8 +12,30 @@ function inputDisabled() {
 }
 
 
+function showButtons() {
+    let isDisabledInput = document.getElementById("input-number-js").disabled
+    if (!isDisabledInput) {
+        document.getElementById("up-down-container-js").style.display = "block"
+    }
+}
+
+function hideButtons() {
+    document.getElementById("up-down-container-js").style.display = "none"
+}
+
+function addNumberButton() {
+    let inputNumberValue = parseInt(document.getElementById("input-number-js").value)
+    if (inputNumberValue === NaN) {
+        inputNumberValue = 0
+    }
+    inputNumberValue++
+    document.getElementById("input-number-js").value = inputNumberValue
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("toggle-disabled-js").addEventListener("click", inputDisabled)
-
+    document.getElementById("input-container-js").addEventListener("mouseover", showButtons)
+    document.getElementById("input-container-js").addEventListener("mouseout", hideButtons)
+    document.getElementById("up-js").addEventListener("click", addNumberButton)
 })
 //document.getElementById("input-number-js").disabled = true
